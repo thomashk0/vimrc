5a6d8cb1983eae8ff70f251708bceb19c3c5d20c
Thomas Hiscock <thomas.hiscock@wanadoo.fr>
2017-11-21 22:27:58 +0100

    improvements:
    * using vim-comment from tpope
    * removing tab bindings

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-
-pushd `dirname $0` > /dev/null
-SCRIPT_PATH=`pwd`
-mkdir -p $SCRIPT_PATH/vim/bundle
+current_dir=$(pwd)
+mkdir -p vim
 
 # Creating symbolik links
-ln -sf $SCRIPT_PATH/vimrc ~/.vimrc
+ln -sf $SCRIPT_PATH/minimal ~/.vimrc
 ln -sf $SCRIPT_PATH/vimrc.bundles ~/.vimrc.bundles
 ln -sf $SCRIPT_PATH/vimrc.bepo ~/.vimrc.bepo
 ln -sf $SCRIPT_PATH/vim ~/.vim
